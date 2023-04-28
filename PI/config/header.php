03f35fa32dab88eb5e3542921dc4e771d50bf7bc
the1scient <guifabbri04@hotmail.com>
2023-04-28 20:21:32 -0300

    renomeando menus e adicionando header

---
+++ PI/config/header.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+        <div class="container-fluid">
+          <a class="logo" href="index.html"> <img src =".\img\logo.png"></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarCollapse" style="margin-left: 45%">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0" style="font-family: 'Montserrat';">
+         
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Início</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href ="#">Catálogo</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Planos</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Central</a>
+              </li>
+            
+              <li class="nav-item" >
+                <a class="nav-link active" aria-current="page" href="#">Entrar / Cadastrar</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
--- PI/registro.php
+++ PI/registro.php
@@ -16,35 +16,9 @@ require_once('global.php');
 
 <body>
     
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-        <div class="container-fluid">
-          <a class="logo" href="index.html"> <img src =".\img\logo.png"></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarCollapse" style="margin-left: 45%">
-            <ul class="navbar-nav me-auto mb-2 mb-md-0" style="font-family: 'Montserrat';">
-         
-              <li class="nav-item">
-                <a class="nav-link" href="index.html">Inicio</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href ="#">Catalogo</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Planos</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Central</a>
-              </li>
-            
-              <li class="nav-item" >
-                <a class="nav-link active" aria-current="page" href="#">Login/ Cadastra-se</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+    <?php 
+    include_once('config/header.php');
+    ?>
     <div class="painel2">
       <form action="controllers/registro.php" method="POST">
         <fieldset>
