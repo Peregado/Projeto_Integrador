a5faad55819d26a3db166d69d8b84964b67ec766
the1scient <guifabbri04@hotmail.com>
2023-05-26 16:46:45 -0300

    arrumando logo do header

--- PI/finalizar.php
+++ PI/finalizar.php
@@ -17,7 +17,7 @@ $dias = $interval->format('%a');
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
-  <title>Senacar</title>
+  <title><?php echo SITE_NAME ?> - Alugar <?php echo $carroInfo['modelo']; ?></title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="../css/style.css" rel="stylesheet" type="text/css">
   <link href="../css/normalize-min.css" rel="stylesheet" type="text/css">
